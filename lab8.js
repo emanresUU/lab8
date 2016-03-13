2068cc4f5ac195562098af813f280d10c3a79c13
Nijel Jones <ni.jones@mail.com>
2016-03-12 16:35:05 -0800

    add addTopping()

--- lab8.js
+++ lab8.js
@@ -3,9 +3,19 @@ var toppingsArray = [];
 function addTopping() {
    "use strict";  
    var topping = document.getElementById("topping").value;
-   toppingsArray.push(topping);
+   
+   if (topping !== toppingsArray[0] && 
+      topping !== toppingsArray[1] && 
+      topping !== toppingsArray[2] && 
+      topping !== toppingsArray[3]) { 
+      toppingsArray.push(topping);
+   }
    var currentToppings = document.getElementById("currentToppings");
-   currentToppings.innerHTML = toppingsArray;
+   var toppingsList = "";
+   for (var i = 0; i < toppingsArray.length; i = i + 1) {
+      toppingsList = toppingsList + "<li>" + toppingsArray[i] + "</li>";
+      currentToppings.innerHTML = toppingsList; 
+   }
 }
 
 
